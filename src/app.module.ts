e3eba78735226bf8594639daf7294e673a9f7bbc
lionnoil682 <lionnoil682@gmail.com>
2025-03-06 15:53:45 +0900

    asd

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,9 @@ import { BldgModule } from './bldg/bldg.module';
         process.env.CLOUDSQL_HOST ||
         `/cloudsql/${process.env.CLOUDSQL_INSTANCE_CONNECTION_NAME}`,
       port: 5432,
-      username: process.env.CLOUD_USER,
-      password: process.env.CLOUD_PASS,
-      database: process.env.CLOUD_DB,
+      username: process.env.CLOUDSQL_USER,
+      password: process.env.CLOUDSQL_PASS,
+      database: process.env.CLOUDSQL_DB,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
     }),
